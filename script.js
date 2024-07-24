6d45b35eec62180ef02a829f5ca7b27f54e95130
Joseph Emmanuel <joseph.emmanuel@outlook.com>
2024-07-24 15:45:37 -0500

    test 5

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var startTime = 123;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
-    height: "0",
-    width: "0",
+    height: "330",
+    width: "330",
     videoId: "7JQS6H2AXdM",
     events: {
       onReady: onPlayerReady,
@@ -10,34 +10,38 @@ function onYouTubeIframeAPIReady() {
   });
 }
 function onPlayerReady(event) {
-  // player.mute();
-  // player.seekTo(startTime);
-  // player.playVideo();
+  player.mute();
+  player.seekTo(startTime);
+  player.playVideo();
   // setTimeout(function () {
   //   player.unMute();
   //   player.playVideo();
-  // }, 100);
+  // }, 1000);
+  player.unMute();
+  player.playVideo();
 }
 
 document.getElementById("startButton").addEventListener("click", function () {
   
-  setTimeout(function () {
-    console.log("startButton clicked:");
-    player.seekTo(startTime);
+   
+  console.log("startButton clicked:");
+  player.seekTo(startTime);
 
-  player.playVideo();
-  }, 4000);
-  
+player.playVideo();
 });
 
 document.getElementById("replayButton").addEventListener("click", function () {
   player.seekTo(0);
   player.playVideo();
 });
-window.addEventListener("load", function() {
-    console.log("Page fully loaded");
-    // onPlayerReady();
-    // player.unMute();
-    const button = document.getElementById('startButton');
-    button.click();
+document.addEventListener('DOMContentLoaded', function() {
+  // Your function here
+  console.log('DOM fully loaded and parsed');
+  const button = document.getElementById('startButton');
+  button.click();
 });
+// window.addEventListener("load", function() {
+//     console.log("Page fully loaded");  
+//     const button = document.getElementById('startButton');
+//     button.click();
+// });
